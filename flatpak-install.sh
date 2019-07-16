553ddf83d39274d960c76b34788ae068cd354680
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-07-16 08:22:26 -0300

    change atom and vscode to pacman install

--- flatpak-install.sh
+++ flatpak-install.sh
@@ -1,8 +1,10 @@
 #!bin/bash
 
-bash scripts/flatpak/atom-install.sh
 bash scripts/flatpak/franz-install.sh
 bash scripts/flatpak/postman-install.sh
 bash scripts/flatpak/spotify-install.sh
-bash scripts/flatpak/visual-studio-code-install.sh
 bash scripts/flatpak/zoom-install.sh
+
+# system integration with linters dont work properly
+# bash scripts/flatpak/atom-install.sh
+# bash scripts/flatpak/visual-studio-code-install.sh
--- pacman-install.sh
+++ pacman-install.sh
@@ -12,6 +12,7 @@ echo "Ranking server list for optimal performance"
 echo "Updating packages before install"
 pacman -Syyu
 
+bash scripts/pacman/atom-install.sh
 bash scripts/pacman/base-devel-install.sh
 bash scripts/pacman/cmake-install.sh
 bash scripts/pacman/ctags-install.sh
@@ -25,14 +26,14 @@ bash scripts/pacman/pamac-install.sh
 bash scripts/pacman/steam-install.sh
 bash scripts/pacman/tig-install.sh
 bash scripts/pacman/tmux-install.sh
+bash scripts/pacman/visual-studio-code-install.sh
 bash scripts/pacman/yay-install.sh
 bash scripts/pacman/xclip-install.sh
 
 echo "changing to fish shell"
 chsh -s /usr/bin/fish
 
-# disabled session 
-# bash scripts/pacman/atom-install.sh
+# disabled session
 # bash scripts/pacman/dbeaver-install.sh
 # bash scripts/pacman/erlang-dependencies-install.sh
 # bash scripts/pacman/mono-install.sh
