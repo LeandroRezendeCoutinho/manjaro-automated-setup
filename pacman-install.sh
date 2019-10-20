86b1864148b3c9eb3f255fa6bce49f0f3bea49d4
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-20 10:41:44 -0300

    update

--- pacman-install.sh
+++ pacman-install.sh
@@ -16,15 +16,16 @@ bash scripts/pacman/atom-install.sh
 bash scripts/pacman/base-devel-install.sh
 bash scripts/pacman/cmake-install.sh
 bash scripts/pacman/ctags-install.sh
+bash scripts/pacman/dbeaver-install.sh
 bash scripts/pacman/fish-install.sh
 bash scripts/pacman/docker-install.sh
-bash scripts/pacman/flatpak-install.sh
 bash scripts/pacman/ncurses-install.sh
 bash scripts/pacman/notepadqq-install.sh
 bash scripts/pacman/opera-install.sh
 bash scripts/pacman/pamac-install.sh
 bash scripts/pacman/steam-install.sh
 bash scripts/pacman/tig-install.sh
+bash scripts/pacman/telegram-desktop-install.sh
 bash scripts/pacman/tmux-install.sh
 bash scripts/pacman/visual-studio-code-install.sh
 bash scripts/pacman/yay-install.sh
@@ -34,12 +35,11 @@ echo "changing to fish shell"
 chsh -s /usr/bin/fish
 
 # disabled session
-# bash scripts/pacman/dbeaver-install.sh
 # bash scripts/pacman/erlang-dependencies-install.sh
+#b ash scripts/pacman/flatpak-install.sh
 # bash scripts/pacman/mono-install.sh
 # bash scripts/pacman/neovim-install.sh
 # bash scripts/pacman/postgresql-install.sh
 # bash scripts/pacman/redis-install.sh
-# bash scripts/pacman/telegram-desktop-install.sh
 # bash scripts/pacman/wine-install.sh
 # bash scripts/pacman/zsh-install.sh
