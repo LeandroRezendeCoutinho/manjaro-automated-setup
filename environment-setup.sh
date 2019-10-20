7b768c1c3babe88e14783ba0ba3108d89f13c404
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-20 12:38:42 -0300

    update

--- environment-setup.sh
+++ environment-setup.sh
@@ -3,7 +3,6 @@
 bash scripts/setup/asdf-setup.sh
 bash scripts/setup/docker-setup.sh
 bash scripts/setup/git-setup.sh
-bash scripts/setup/fish-setup.sh
 bash scripts/setup/ruby-setup.sh
 bash scripts/setup/teamviewer-setup.sh
 bash scripts/setup/tmux-setup.sh
@@ -11,5 +10,6 @@ bash scripts/setup/tmux-setup.sh
 # optional
 # bash scripts/setup/erlang-setup.sh
 # bash scripts/setup/elixir-setup.sh
+# bash scripts/setup/neovim-setup.sh
 # bash scripts/setup/postgresql-setup.sh
 # bash scripts/setup/zsh-setup.sh
