5ba9bfd8b5c861cc6a386eaeaba9fee5372261c9
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-20 10:40:58 -0300

    update ruby install

--- scripts/setup/ruby-setup.sh
+++ scripts/setup/ruby-setup.sh
@@ -2,6 +2,14 @@
 
 asdf plugin-add ruby
 
-asdf install ruby 2.6.3
+asdf install ruby 2.6.5
 
-asdf global ruby 2.6.3
+asdf global ruby 2.6.5
+
+gem install bundler
+gem install rubocop
+gem install reek
+gem install solargraph
+gem install fasterer
+gem install debride
+gem install ruby-lint
